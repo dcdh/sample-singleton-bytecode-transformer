71696867b903752a873b51b8a2c5bb7e7d552955
damien <damien.clementdhuart@gmail.com>
2024-06-16 17:41:34 +0200

    fixed by using AdditionalBeanBuildItem

--- deployment/src/main/java/com/damdamdeo/sample/singleton/bytecode/transformer/deployment/SampleSingletonBytecodeTransformerProcessor.java
+++ deployment/src/main/java/com/damdamdeo/sample/singleton/bytecode/transformer/deployment/SampleSingletonBytecodeTransformerProcessor.java
@@ -1,6 +1,8 @@
 package com.damdamdeo.sample.singleton.bytecode.transformer.deployment;
 
 import com.damdamdeo.sample.singleton.bytecode.transformer.runtime.CommandHandler;
+import io.quarkus.arc.deployment.AdditionalBeanBuildItem;
+import io.quarkus.arc.processor.DotNames;
 import io.quarkus.deployment.annotations.BuildProducer;
 import io.quarkus.deployment.annotations.BuildStep;
 import io.quarkus.deployment.builditem.ApplicationIndexBuildItem;
@@ -17,6 +19,7 @@ class SampleSingletonBytecodeTransformerProcessor {
         return new FeatureBuildItem(FEATURE);
     }
 
+    /* not working
     @BuildStep
     void registerCommandHandlersAsSingletonBean(final ApplicationIndexBuildItem applicationIndexBuildItem,
                                                 final BuildProducer<BytecodeTransformerBuildItem> bytecodeTransformerBuildItemProducer) {
@@ -29,6 +32,21 @@ class SampleSingletonBytecodeTransformerProcessor {
                                         .setVisitorFunction((s, classVisitor) -> // NOT CALLED :/
                                                 new AddSingletonAnnotationClassVisitor(classVisitor))
                                         .setCacheable(true)
+                                        .build()
+                        )
+                );
+    }*/
+    @BuildStep
+    void registerCommandHandlersAsSingletonBean(final ApplicationIndexBuildItem applicationIndexBuildItem,
+                                                final BuildProducer<AdditionalBeanBuildItem> additionalBeanBuildItemProducer) {
+        applicationIndexBuildItem.getIndex()
+                .getAllKnownImplementors(DotName.createSimple(CommandHandler.class))
+                .forEach(classInfo ->
+                        additionalBeanBuildItemProducer.produce(
+                                new AdditionalBeanBuildItem.Builder()
+                                        .addBeanClasses(classInfo.name().toString())
+                                        .setUnremovable()
+                                        .setDefaultScope(DotNames.SINGLETON)
                                         .build()
                         )
                 );
